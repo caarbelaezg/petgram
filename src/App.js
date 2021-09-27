8cf524ec38ca40af61947b569d3ebf66752a548a
Carlos Arbelaez <caarbelaezg@gmail.com>
2021-09-27 09:27:36 -0500

    custom hooks and styled components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './styles/GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
+
+export const App = () => (
+  <>
+    <Logo />
+    <GlobalStyles />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+
+  </>
+)
---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react'
+import { ImgWrapper, Img, Button } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
+
+const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+
+  useEffect(() => {
+    console.log(element)
+  }, [element])
+
+  return (
+    <article ref={element}>
+      <a href={`/detail/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
+      </a>
+      <Button>
+        <MdFavoriteBorder size='32px' /> {likes} likes!
+      </Button>
+    </article>
+  )
+}
